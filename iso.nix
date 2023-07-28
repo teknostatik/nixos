0e47825901c48b392022a2ee29d9d03eff744c85
Andy Ferguson <andy@teknostatik.org>
2023-07-28 22:06:50 +0100

    File to build an iso

---
+++ iso.nix
@@ -0,0 +1,55 @@
+# This module defines a small NixOS installation CD.  It does not
+# contain any graphical stuff.
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix>
+
+    # Provide an initial copy of the NixOS channel so that the user
+    # doesn't need to run "nix-channel --update" first.
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+  
+    # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  environment.systemPackages = with pkgs; [
+  git
+  featherpad
+  pandoc
+  dropbox
+  vscode
+  dmenu
+  feh
+  i3lock
+  kitty
+  i3blocks
+  arandr 
+  scrot 
+  xautolock 
+  barrier
+  imagemagick
+  neofetch
+  zathura
+  pcmanfm
+  htop
+  blueman
+  cowsay
+  fortune
+  shellcheck
+  abiword
+  rhythmbox
+  brasero
+  sound-juicer
+  transmission
+  byobu
+  tmux
+  screen
+  qmk
+  networkmanagerapplet
+  ];
+  
+}
+
+
