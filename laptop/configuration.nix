a28b29a0f7e48dfe20871244894b437aafc13835
Andy Ferguson <andy@teknostatik.org>
2023-09-16 08:38:45 +0100

    Enable qmk - maybe

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -108,6 +108,13 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # enable QMK to write to keyboards
+
+  {
+  hardware.keyboard.qmk.enable = true
+}
+
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
